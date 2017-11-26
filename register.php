d97896e8d3aa026c974f3cbf0e4d4b27579be941
Massimiliano Rak <maxrak@MacBook-Pro-di-Massimiliano.local>
2017-11-26 12:24:49 +0100

    updated register information

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://rktest1.herokuapp.com/execute.php';
+$BOT_TOKEN = '505686531:AAEyjaDCKtLclQJb6-Dm4SOJ3cl_MjSvW9M';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
