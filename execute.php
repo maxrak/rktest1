cc7d6a34f5fffc921d3562d237022f62efc4993e
Massimiliano Rak <maxrak@MacBook-Pro-di-Massimiliano.local>
2017-11-26 21:37:33 +0100

    update bot

--- execute.php
+++ execute.php
@@ -7,6 +7,9 @@ function pg_connection_string_from_database_url() {
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
 if(!$update)
 {
   exit;
@@ -27,22 +30,18 @@ if ($request == "manca")
 {
 	$text += "mancano:\n";
 
-	# Here we establish the connection. Yes, that's all.
-	$pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 	# Now let's use the connection for something silly just to prove it works:
 	$result = pg_query($pg_conn, "SELECT * FROM dispensa.manca");
 
-	print "<pre>\n";
 	if (!pg_num_rows($result)) {
-		print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+		$text +="Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n";
 	} else {
-  		print "Tables in your database:\n";
+  		$text += "Tables in your database:\n";
   		while ($row = pg_fetch_row($result)) { 
   			$text += "- $row[1]\n"; 
   		}
 	}
-	print "\n";
 } else {
 	$text= "illaltro";	
 }
